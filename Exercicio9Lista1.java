9dd5cb1a29bf075d3b8ef5fd3d26c1b07b6a7b34
Neto Russo <netoacrn1@gmail.com>
2024-06-25 14:52:08 -0300

    subindo o exercicio 9

---
+++ Exercicio9Lista1.java
@@ -0,0 +1,22 @@
+package ExerciciosLista1;//        9. Escreva um programa que verifica se uma string é um palíndromo.
+
+import java.util.Scanner;
+
+public class Exercicio9Lista1 {
+    public static void main(String[] args) {
+        //9. Escreva um programa que verifica se uma string é um palíndromo.
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite uma frase: ");
+        String frase = entrada.nextLine();
+        frase = frase.toLowerCase();
+
+        String palindromo = new StringBuilder(frase).reverse().toString();
+
+        String resposta = (frase.equals(palindromo)) ? "A frase : " + frase + " é um palíndromo" : "A frase : " + frase + " não é um palíndromo";
+        System.out.println(resposta);
+
+
+        entrada.close();
+    }
+}
